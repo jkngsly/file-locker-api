8d2359a038db06166bb62f488009bb57e8ab177d
Jordan Kingsley <jkngsly@gmail.com>
2025-01-07 23:04:47 -0500

    Add User module

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,19 @@
+import { isEmail, IsNotEmpty, IsString } from 'class-validator'
+
+export class CreateUserDto { 
+    @IsNotEmpty()
+    @IsString()
+    firstName: string;
+
+    @IsNotEmpty()
+    @IsString()
+    lastName: string;
+
+    @IsNotEmpty()
+    //@isEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+}
